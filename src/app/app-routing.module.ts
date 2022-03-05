210eb63594cf0ebf0f64b6b632e040f46acf611f
sanjay kumar <sanjayamystery@gmail.com>
2022-03-05 14:50:58 +0530

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/posts',pathMatch:'full'},
-  {path:'login',component:LoginComponent},
-  {path:'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule)},
-  {path:'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule)},
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
+  { path: 'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule) },
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
