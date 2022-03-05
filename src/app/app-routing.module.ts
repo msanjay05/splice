c767028e5f7eb5389cab3fc21814615acce83a6d
sanjay kumar <sanjayamystery@gmail.com>
2022-03-05 14:05:34 +0530

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'/posts',pathMatch:'full'},
   {path:'login',component:LoginComponent},
   {path:'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule)},
   {path:'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule)},
